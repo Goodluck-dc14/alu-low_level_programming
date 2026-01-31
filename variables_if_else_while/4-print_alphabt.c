5393c9730c96f19f748126c64347539cc82c2fb0
Goodluck-dc14 <ogbonnaiheoma48@gmail.com>
2026-01-31 10:27:35 -0800

    Final commit

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
+
